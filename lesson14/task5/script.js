83fef571e3548d3830180352061ae24b42b64b7e
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2023-12-14 17:27:55 +0300

    Add 4 task for lesson 14. RegExp change vowels to index

---
+++ lesson14/task5/script.js
@@ -0,0 +1,21 @@
+function changeVowels(str) {
+    let strMatch = [...str.matchAll(/[aeiouy]|[аеёиоуыэюя]/g)];
+    let strArr = str.split('');
+
+    for (let i = 0; i < strMatch.length; i++) {
+        for (let j = 0; j < strArr.length; j++) {
+            if (strArr[j] == strMatch[i][0]) {
+                strArr[j] = strMatch[i].index
+                break;
+            }
+        }
+    }
+
+    return strArr.join("");
+}
+
+
+console.log(changeVowels("aboba"));
+console.log(changeVowels("привет"));
+console.log(changeVowels("Здр3вств8йт11 р14б16т18!"));
+console.log(changeVowels("Здр3вств8йт11 р14б16т18!") == "Здр3вств8йт11 р14б16т18!");
