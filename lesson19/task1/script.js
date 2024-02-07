f3ddb1b4e5176d4ced347b90770c99a2b476f7e0
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-02-07 15:38:09 +0300

    Add task 1 for lesson 19. Valid JSON

---
+++ lesson19/task1/script.js
@@ -0,0 +1,18 @@
+function isValidJson(str) {
+    if (!str || typeof str !== 'string') return false
+    try {
+        JSON.parse(str)
+        return true
+    } catch (e) {
+        return false
+    }
+}
+
+let s1 = `{"name":"julie","age":15}`
+let s2 = `{name:"julie";"age":15}`
+let s3 = `i'm just string`
+let s4 = 123
+let s5 = true
+let s6;
+
+console.log([s1, s2, s3, s4, s5].map(isValidJson))
